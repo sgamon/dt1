cc9bd9593eb5293cfa43201d2da3b301a1f709aa
Scott Gamon <sgamon@gmail.com>
2020-04-12 23:28:36 -0700

    tidy main.js

--- main.js
+++ main.js
@@ -4,20 +4,7 @@ defineComponents([
   'enemies-panel', 'random-enemy', 'd-6', 'dice-panel', 'breadcrumb-panel'
 ]);
 
-window.addEventListener('DOMContentLoaded', (event) => {
-});
+// window.addEventListener('DOMContentLoaded', (event) => {
+// });
 
 //-----------------------------------------------------------------------------
-
-function preventDefault(e) {
-  if (e && e.preventDefault) {
-    e.preventDefault();
-  }
-}
-
-function addEvent(elementId, eventName, fn) {
-  document.getElementById(elementId).addEventListener(eventName, fn);
-}
-
-//-----------------------------------------------------------------------------
-
